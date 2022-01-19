34034d4d0969990d6a661b5c8a3bbd7e956ce8be
Qwertyizm <stanislaw.sochacki@outlook.com>
2022-01-19 10:26:39 +0100

    Add description of functions

--- wielomiany.h
+++ wielomiany.h
@@ -6,41 +6,41 @@ struct Wielomian3d {
     int size;  //rozmiar ogólny
     int var[3]; //rozmiar poszczególnych zmiennych
 
-    double *val;
+    double *val; //reprezentacja wartości przy potęgach zmiennych
 };
 
 
-int max(int a, int b);
+int max(int a, int b);  //wyznacza max z dwóch liczb
 
-char *print(wielomian v, bool delete);
+char *print(wielomian v, bool delete); //zwraca reprezentację wielomiany jako ciąg znaków
 
-double compute(wielomian v, wielomian x, wielomian y, wielomian z, bool delete);
+double compute(wielomian v, wielomian x, wielomian y, wielomian z, bool delete); //wyznacza wartość wielomianu w punkcie
 
-wielomian copy(wielomian x);
+wielomian copy(wielomian x); //kopiuje wielomian
 
-wielomian add(wielomian v, wielomian y);
+wielomian add(wielomian v, wielomian y); //dodaje do siebie dwa wielomiany
 
-wielomian sub(wielomian v, wielomian y);
+wielomian sub(wielomian v, wielomian y); //odejmuje wielomian od wielomianu
 
-wielomian multiply(wielomian v, wielomian y);
+wielomian multiply(wielomian v, wielomian y); //mnoży dwa wielomiany
 
-wielomian divide(wielomian v, wielomian y, wielomian r, bool delete);
+wielomian divide(wielomian v, wielomian y, wielomian r, bool delete); //dzieli wielomian przez wielomian
 
-bool greater_than_zero(wielomian v);
+bool greater_than_zero(wielomian v); //sprawdza czy wielomian jest różny od zera
 
-wielomian nwd(wielomian v, wielomian y);
+wielomian nwd(wielomian v, wielomian y); //wyznacza największy wspólny dzielnik
 
-wielomian derivative(wielomian v, bool delete);
+wielomian derivative(wielomian v, bool delete); //wyznacza pochodną wielomianu jednej zmiennej
 
-wielomian m_zero(wielomian v);
+wielomian m_zero(wielomian v); //wyznacza miejsce zerowe wielomianu jednej zmiennej
 
-wielomian from_d_to_w(double d);
+wielomian from_d_to_w(double d); //tworzy strukturę wielomian z liczby
 
-void del(wielomian v);
+void del(wielomian v); //dealokuje pamięć zajmowaną przez wielomian
 
-void uprosc(wielomian v);
+void uprosc(wielomian v); //upraszcza zapis wielomianu
 
-int from_kart_to_linear(int x, int y, int z, int xs, int ys);
+int from_kart_to_linear(int x, int y, int z, int xs, int ys); //wyznacza położenie punktu w liniowej reprezentacji przestrzeni 3d
 
 
 
